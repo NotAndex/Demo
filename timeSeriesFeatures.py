012812aea408bc18aabf2fba4dca35b862bbae96
NotAndex <85022950+NotAndex@users.noreply.github.com>
2021-05-29 20:06:47 +0200

    Add files via upload

--- timeSeriesFeatures.py
+++ timeSeriesFeatures.py
@@ -3,11 +3,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-
+import yfinance as yf
 #%%
-
 
 def addTimeFeatures(data):
     
@@ -15,19 +12,22 @@ def addTimeFeatures(data):
     data['year']    = data['Date'].dt.year
     data['quarter'] = data['Date'].dt.quarter
     data['month']   = data['Date'].dt.month
-    label_encoder   = LabelEncoder()
-    data = pd.concat([data,pd.DataFrame(to_categorical(label_encoder.fit_transform(data['month'])))],axis=1)
+    monthCat = pd.DataFrame(to_categorical(data.loc[:,['month']], num_classes=13))
+    data = pd.concat([data,monthCat.loc[:,1:]],axis=1)
     data['day']     = data['Date'].dt.day
     data['wday']    = data['Date'].dt.dayofweek
-    data = pd.concat([data,pd.DataFrame(to_categorical(data['wday']),columns=["Monday","Tuesday","Wednesday","Thursday","Friday"])],axis=1)
-    
+    dayCat = pd.DataFrame(to_categorical(data.loc[:,['wday']], num_classes=8),
+                          columns=["Monday","Tuesday","Wednesday",
+                                   "Thursday","Friday","Saturday","Sunday","wday"])
+    data = pd.concat([data,dayCat.iloc[:,:-1]],axis=1)
     data = data.drop('Date',1) 
-
     cols = list(data)
     cols.insert(0, cols.pop(cols.index('Close')))
     data = data.loc[:, cols]
     
     return(data)
+
+
 #%%
 def addSMAFeature(data, colName, window_size):
     data[f'{colName} {window_size}d_sma'] = data[colName].rolling(window=window_size, min_periods=1).mean()
